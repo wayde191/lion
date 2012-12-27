07b676a4f6f386e59f41d80884e2eb7a0446d1a4
wayde191 <hakula567>
2012-12-27 15:34:04 +0800

    Add gantt source

---
+++ js/gantt/GanttViewController.js
@@ -0,0 +1,10 @@
+
+/*
+ * Copyright (c) 2012-2020, iHakula Studio Software Inc.
+ * Use, modification, and distribution subject to terms of license.
+ * @version: 1.0
+ * @date: 12/27/2012
+ * @author: Wayde Sun
+ * @email: hakula@ihakula.com
+ * @website: www.ihakula.com
+ */
