f0c06e1ffb9f55b5420ba0d5b1b284347aa67561
wayde191 <hakula567>
2012-12-31 15:44:20 +0800

    Add gantt
    gantt added

--- js/gantt/GanttViewController.js
+++ js/gantt/GanttViewController.js
@@ -8,3 +8,44 @@
  * @email: hakula@ihakula.com
  * @website: www.ihakula.com
  */
+
+ih.defineClass("ih.GanttViewController", null, null, function(GANTT, gantt){
+
+    gantt.prototype.init = function(){
+        this.datamodel = new ih.GanttDataModel();
+        this.datamodel.delegate = this;
+    };
+
+    gantt.prototype.viewDidLoad = function(){
+    console.log("jajajaj");
+        //this.bindBtnsEvent();
+    };
+
+
+    gantt.prototype.bindBtnsEvent = function(){
+        var self = this;
+        $("#awards").click(ih.$F(self.onControllerBtnClicked).bind(self));
+        $("#suggestion").click(ih.$F(self.onControllerBtnClicked).bind(self));
+        $("#improve").click(ih.$F(self.onControllerBtnClicked).bind(self));
+        $("#advice").click(ih.$F(self.onControllerBtnClicked).bind(self));
+        $("#goodjob").click(ih.$F(self.onControllerBtnClicked).bind(self));
+    };
+
+    gantt.prototype.showMsg = function(dialogMsg){
+      // Dialog
+        $('#dialog').dialog({
+            autoOpen: false,
+            width: 600,
+            title: dialogMsg.title,
+            buttons: {
+                "确定": function() {
+                    $(this).dialog("close");
+                }
+            }
+        });
+
+        // Dialog Link
+        $('#dialog').html(dialogMsg.text).dialog('open');
+    };
+
+});
