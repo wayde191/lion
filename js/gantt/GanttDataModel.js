7af3692b6ef2ff10f8e84a585acf988af60a1e53
wayde191 <hakula567>
2013-01-06 23:34:25 +0800

    ttt

--- js/gantt/GanttDataModel.js
+++ js/gantt/GanttDataModel.js
@@ -51,7 +51,7 @@ ih.defineClass("ih.GanttDataModel", null, null, function(GANTT, gantt){
                 if (1 == response.status) {
                    this.tasks = response.data;
                    this.totalPageNum = response.totalPage;
-                   $("#totalPageNumber").val(this.totalPageNum);
+                   $("#totalPageNumber").attr("value", this.totalPageNum);
                    console.log(this.tasks);
                     $('[rel*="data{menuitem}"]').setData({
                       menuitem : response.data
